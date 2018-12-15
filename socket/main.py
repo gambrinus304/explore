1eb220b7ef30b6078d74bca4d5aa0024b652f703
gambrinus304 <devilhunter2009@mail.ru>
2018-12-15 04:52:14 +0300

    Study the work of the sockets

---
+++ socket/main.py
@@ -0,0 +1,81 @@
+import socket
+from views import *
+
+URLS = {
+    '/': index,
+    '/blog': blog
+}
+
+
+
+def parse_request(request):
+    parsed = request.split(' ')
+    method = parsed[0]
+    url = parsed[1]
+    return(method, url)
+
+
+
+def generate_headers(method, url):
+    if not method =='GET':
+        return ('HTTP/1.1 405 Method not allowed\n\n', 405)
+
+    if not url in URLS:
+        return ('HTTP/1.1 404 Not found\n\n', 404)
+
+    return ('HTTP/1.1 200 OK\n\n', 200)
+
+
+def generate_content(code, url):
+    if code == 404:
+        return '<h1>404</h1><p>Not found</p>'
+    if code == 405:
+        return '<h1>405</h1><p>Method not allowed</p>'
+    return (URLS[url])()
+
+
+
+def generate_response(request):
+    method, url = parse_request(request)
+    headers, code = generate_headers(method, url)
+    body = generate_content(code, url)
+
+
+    return (headers + body).encode()
+
+
+
+def run():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # AF == adress server, INET == IP. SOCK_STREAM == TCP
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # set reuse socket
+    server_socket.bind(('localhost', 5000))
+    # connect server_socket with address:port
+    server_socket.listen()
+    # run server to listen port
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        # server_socket == get function
+        request = client_socket.recv(1024)
+        print(request)
+        # print(request.decode('utf-8'))
+        # recive package
+        print()
+        print(addr)
+
+
+        try:
+            response = generate_response(request.decode('utf-8'))
+        except:
+            print()
+
+
+        client_socket.sendall(response)
+        # encode = transform string to bites
+        client_socket.close()
+
+
+if __name__ == "__main__":
+    run()
