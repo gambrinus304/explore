d947487f3b12bb6b846893ced333c3fe95ab6886
gambrinus304 <devilhunter2009@mail.ru>
2019-01-14 03:03:07 +0300

    Parsing, 2

---
+++ parsing_2.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+def refined(s):
+    r = s.split(' ')[0]
+    return r.replace(',', '')
+
+
+def write_csv(data):
+    with open('plugins.csv', 'a') as f:
+        writer = csv.writer(f)
+
+        # second () for write data to tuple ( korteje :) ) 
+        writer.writerow((data['name'],
+                        data['url'],
+                        data['reviews']))
+
+def get_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    popular = soup.find_all('section')[3]
+    plugins = popular.find_all('article')
+
+    for plugin in plugins:
+        name = plugin.find('h2').text
+        url = plugin.find('h2').find('a').get('href')
+
+        r = plugin.find('span', class_='rating-count').find('a').text
+        rate = refined(r)
+
+
+        data = {'name': name,
+                'url': url,
+                'reviews': rate}
+
+        write_csv(data)
+
+def main():
+    url = 'https://wordpress.org/plugins/'
+    get_data(get_html(url))
+    
+
+
+if __name__ == "__main__":
+    main()
