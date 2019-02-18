19ad75db11e77294a694a7f17be0c139abab7873
gambrinus304 <devilhunter2009@mail.ru>
2019-02-19 02:41:34 +0300

    Parsing, 12

---
+++ parsing_12.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+from random import choice
+
+
+
+def get_proxy():
+    proxi_url = 'https://free-proxy-list.net/'
+    html = requests.get(proxi_url).text
+    soup = BeautifulSoup(html, 'lxml')
+
+    trs = soup.find('table', id='proxylisttable').find_all('tr')[1:11]
+    proxies = []
+
+    for tr in trs:
+        tds = tr.find_all('td')
+        ip = tds[0].text.strip()
+        port = tds[1].text.strip()
+        schema = 'https' if 'yes' in tds[6].text.strip() else 'http'
+        proxy = {'schema': schema, 'address': ip + ':' + port}
+        proxies.append(proxy)
+
+    return choice(proxies)
+        
+
+def get_html(url):
+    # proxies = {'https': 'ipadress:5000'}
+    p = get_proxy() # {'schema': '', 'address': ::}
+    proxy = {p['schema']: p['address']}
+    r = requests.get(url, proxies = proxy, timeout=5)
+    
+    return r.json()['origin']
+        
+
+def main():
+    url = 'http://httpbin.org/ip'
+    print(get_proxy())
+    print(get_html(url))
+
+
+
+if __name__ == "__main__":
+    main()
